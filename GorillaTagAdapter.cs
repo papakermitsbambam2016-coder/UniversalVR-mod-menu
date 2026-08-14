18f5102a217146d019f8c3041f6e44607dd6e170
papakermitsbambam2016-coder <papakermitsbambam2016@gmail.com>
2026-08-14 18:48:11 -0400

    Create GorillaTagAdapter.cs

---
+++ GorillaTagAdapter.cs
@@ -0,0 +1,4 @@
+namespace UniversalVR.Adapters.GorillaTag;
+public sealed class GorillaTagAdapter:StubAdapterBase {
+ public override string Id=>"gorilla-tag"; public override string DisplayName=>"Gorilla Tag";
+}
