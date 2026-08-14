9c5a01bd9f9839eec12a205c4f88f385e239e864
papakermitsbambam2016-coder <papakermitsbambam2016@gmail.com>
2026-08-14 18:49:11 -0400

    Create ScaryBaboonAdapter.cs

---
+++ ScaryBaboonAdapter.cs
@@ -0,0 +1,4 @@
+namespace UniversalVR.Adapters.ScaryBaboon;
+public sealed class ScaryBaboonAdapter:StubAdapterBase {
+ public override string Id=>"scary-baboon"; public override string DisplayName=>"Scary Baboon";
+}
