c03c910dc646689faa003b277e1eb17088276bca
papakermitsbambam2016-coder <papakermitsbambam2016@gmail.com>
2026-08-29 16:15:03 -0400

    Restore Chainsaw code mod Config

---
+++ Chainsaw BONELAB Code Mod/ChainsawCodeMod/Config.cs
@@ -0,0 +1,8 @@
+namespace ChainsawBONELABCodeMod
+{
+    internal static class Config
+    {
+        public const float BladeDegreesPerSecond = 1800f;
+        public const float ScanIntervalSeconds = 2f;
+    }
+}
