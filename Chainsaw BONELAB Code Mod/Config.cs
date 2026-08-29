b96fc4101f14ef0282af787dae6d3fc5db4c0383
papakermitsbambam2016-coder <papakermitsbambam2016@gmail.com>
2026-08-29 16:34:10 -0400

    Finish Chainsaw code mod config

---
+++ Chainsaw BONELAB Code Mod/Config.cs
@@ -0,0 +1,9 @@
+namespace ChainsawBONELABCodeMod
+{
+    internal static class Config
+    {
+        public const float BladeDegreesPerSecond = 1800f;
+        public const float Damage = 7.5f;
+        public const float DamageInterval = 0.075f;
+    }
+}
