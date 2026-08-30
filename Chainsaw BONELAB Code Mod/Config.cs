fb607c5532c995064c598ffefb4dde0fef3d470f
papakermitsbambam2016-coder <papakermitsbambam2016@gmail.com>
2026-08-30 16:57:14 -0400

    Refactor Config class and add new settings
    Updated Config class to use static fields and added new options for MotorSound and DebugMode.

--- Chainsaw BONELAB Code Mod/Config.cs
+++ Chainsaw BONELAB Code Mod/Config.cs
@@ -2,8 +2,16 @@ namespace ChainsawBONELABCodeMod
 {
     internal static class Config
     {
-        public const float BladeDegreesPerSecond = 1800f;
-        public const float Damage = 7.5f;
-        public const float DamageInterval = 0.075f;
+        public static bool Enabled = true;
+
+        public static float BladeDegreesPerSecond = 1800f;
+
+        public static float Damage = 7.5f;
+
+        public static float DamageInterval = 0.075f;
+
+        public static bool MotorSound = true;
+
+        public static bool DebugMode = false;
     }
 }
